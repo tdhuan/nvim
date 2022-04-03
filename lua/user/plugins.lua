6de45a5893821075006c054f104051abb8ef9be2
tdhuan <dinhhuantran3010@gmail.com>
2022-04-04 00:38:47 +0700

    Add plugin packer.nvim

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -28,8 +28,7 @@ packer.init {
 
 return require('packer').startup(function(use)
   -- My plugins here
-  -- use 'foo1/bar1.nvim'
-  -- use 'foo2/bar2.nvim'
+  use 'wbthomason/packer.nvim'
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
