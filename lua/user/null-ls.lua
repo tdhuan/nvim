6ef96bd486f2e8937720732b196d7afbd855e3c4
tdhuan <dinhhuantran3010@gmail.com>
2022-04-15 01:34:08 +0700

    Add auto format in null-ls

--- lua/user/null-ls.lua
+++ lua/user/null-ls.lua
@@ -17,5 +17,16 @@ local sources = {
 }
 
 null_ls.setup({
+	debug = true,
 	sources = sources,
+	on_attach = function(client)
+		if client.resolved_capabilities.document_formatting then
+			vim.cmd([[
+            augroup LspFormatting
+                autocmd! * <buffer>
+                autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()
+            augroup END
+            ]])
+		end
+	end,
 })
