808c28090cf794d35f5d9b481cc42dc56f9517e0
tdhuan <dinhhuantran3010@gmail.com>
2022-04-15 12:37:43 +0700

    format code

--- init.lua
+++ init.lua
@@ -1,20 +1,20 @@
-require "user.options"
-require "user.keymaps"
-require "user.plugins"
-require "user.lsp"
-require "user.cmp"
-require "user.gitsigns"
-require "user.lualine"
-require "user.bufferline"
-require "user.comment"
+require("user.options")
+require("user.keymaps")
+require("user.plugins")
+require("user.lsp")
+require("user.cmp")
+require("user.gitsigns")
+require("user.lualine")
+require("user.bufferline")
+require("user.comment")
 
 local cmd = vim.cmd
 local api = vim.api
 
-cmd [[filetype plugin indent on]]
-cmd [[syntax enable]]
+cmd([[filetype plugin indent on]])
+cmd([[syntax enable]])
 
-api.nvim_set_var('gruvbox_material_palette', 'mix')
-api.nvim_set_var('gruvbox_material_background', 'hard')
-api.nvim_set_var('g:gruvbox_material_better_performance', '1')
-cmd [[colorscheme gruvbox-material]]
+api.nvim_set_var("gruvbox_material_palette", "mix")
+api.nvim_set_var("gruvbox_material_background", "hard")
+api.nvim_set_var("g:gruvbox_material_better_performance", "1")
+cmd([[colorscheme gruvbox-material]])
